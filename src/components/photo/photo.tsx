a00b284faaa68b11284da2ae3adfda65145dd9b3
guskirb <anguskirby1998@gmail.com>
2024-06-28 23:38:52 +0100

    Set up react environment

---
+++ src/components/photo/photo.tsx
@@ -0,0 +1,9 @@
+import "./photo.css"
+
+export default function Photo({ photo }) {
+
+
+  return (
+    <img className="photo" src={photo}></img>
+  )
+}
---
+++ src/pages/game/game.tsx
@@ -0,0 +1,10 @@
+import Photo from "../../components/photo/photo";
+import wallyEasy from "../../assets/wally-easy.jpg"
+import wallyMedium from "../../assets/wally-medium.jpg"
+
+export default function Game() {
+
+  return (
+    <Photo photo={wallyMedium}/>
+  )
+}
