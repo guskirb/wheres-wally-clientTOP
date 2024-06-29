df8840aabb02eb86e646248e27cc035278203529
guskirb <anguskirby1998@gmail.com>
2024-06-29 18:24:53 +0100

    Layout & style changes

--- src/components/tracker/tracker.tsx
+++ src/components/tracker/tracker.tsx
@@ -1,11 +1,11 @@
 import "./tracker.css";
 
-export default function Tracker() {
+export default function Tracker({ characters }) {
   return (
     <div className="tracker__container">
-      <div className="tracker-img"></div>
-      <div className="tracker-img"></div>
-      <div className="tracker-img"></div>
+      <div className="tracker-img">{characters[0].name} {characters[0].found ? "O" : "X"}</div>
+      <div className="tracker-img">{characters[1].name} {characters[1].found ? "O" : "X"}</div>
+      <div className="tracker-img">{characters[2].name} {characters[2].found ? "O" : "X"}</div>
     </div>
   );
 }
